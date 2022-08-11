16242951a6fad1f0b480e6e5352b1fdbf98cd7e5
kiril <kirillusichenko@gmail.com>
2022-08-11 17:53:28 +0300

    fix: change email placeholder, onboarding

--- src/screens/secondOnboarding/SecondOnboarding.tsx
+++ src/screens/secondOnboarding/SecondOnboarding.tsx
@@ -1,99 +1,108 @@
-import React, {useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import styled from 'styled-components';
-import Button from '../../components/shared/button/Button';
-import {Container} from '../../components/shared/container/Container';
-import {State} from "../../store";
-import {AppDispatch} from "../../App";
-import {setUserName, updateEmail} from '../../store/actions/user';
-import {Navigate, useNavigate} from 'react-router-dom';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { AppDispatch } from "../../App";
+import Button from "../../components/shared/button/Button";
+import { Container } from "../../components/shared/container/Container";
 import Header from "../../components/shared/header/Header";
+import { State } from "../../store";
+import { setUserName, updateEmail } from "../../store/actions/user";
 
 const SecondOnboarding = () => {
-    const user = useSelector((state: State) => state.userReducer.user)
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [loading, setLoading] = useState(false)
-    const dispatch = useDispatch<AppDispatch>()
-    const nav = useNavigate()
+  const user = useSelector((state: State) => state.userReducer.user);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch<AppDispatch>();
+  const nav = useNavigate();
 
-    const onNextClick = async (email: string | null | undefined) => {
-        setLoading(true)
-        await dispatch(setUserName(name))
-        setLoading(false)
-        if (email) nav('/dashboard')
+  const onNextClick = async (email: string | null | undefined) => {
+    setLoading(true);
+    await dispatch(setUserName(name));
+    setLoading(false);
+    if (email) nav("/dashboard");
+  };
+  console.log("onboarding");
 
-    }
-    console.log('onboarding');
-    
-    const onSeeClick = async () => {
-        setLoading(true)
-        await dispatch(updateEmail(email))
-        setLoading(false)
-        nav('/dashboard')
-    }
-    return (
-        <section style={{height: '100%'}}>
-            {
-                user ? (
-                    user.full_name ? (
-                        user.email ? (<Navigate to='/dashboard'/>) : (
-                            <Wrapper>
-                                <div>
-                                    <Header/>
-                                    <Container>
-                                        <HeadingEmail>
-                                            Hey there, {user.full_name}! 👋
-                                        </HeadingEmail>
-                                        <Input type="email"
-                                               placeholder='the.real.jane.smith@gmail.com'
-                                               value={email}
-                                               onChange={(e) => setEmail(e.target.value)}
-                                        />
+  const onSeeClick = async () => {
+    setLoading(true);
+    await dispatch(updateEmail(email));
+    setLoading(false);
+    nav("/dashboard");
+  };
+  return (
+    <section style={{ height: "100%" }}>
+      {user ? (
+        user.full_name ? (
+          user.email ? (
+            <Navigate to="/dashboard" />
+          ) : (
+            <Wrapper>
+              <div>
+                <Header />
+                <Container>
+                  <HeadingEmail>Hey there, {user.full_name}! 👋</HeadingEmail>
+                  <Input
+                    type="email"
+                    placeholder={`What's your email?`}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
 
-                                        <Button onClick={onSeeClick} isLoading={loading} disabled={!email.length}>See
-                                            your
-                                            photos!
-                                        </Button>
-                                    </Container>
-                                </div>
+                  <Button
+                    onClick={onSeeClick}
+                    isLoading={loading}
+                    disabled={!email.length}
+                  >
+                    See your photos!
+                  </Button>
+                </Container>
+              </div>
 
-
-                                <Container>
-                                    <Footer>
-                                        By continuing, you indicate that you have read and agree to our <Underlined>Terms
-                                        of
-                                        Use</Underlined> & <Underlined>Privacy Policy</Underlined>
-                                    </Footer>
-                                </Container>
-                            </Wrapper>
-                        )
-
-                    ) : (
-                        <div>
-                            <Header/>
-                            <Container>
-                                <Heading>
-                                    Let’s get to know you
-                                </Heading>
-                                <Input value={name} onChange={(e) => setName(e.target.value)} type="text"
-                                       placeholder='What’s your name?'/>
-                                {user.email ?
-                                    <Button isLoading={loading}
-                                            disabled={!name.length}
-                                            onClick={() => onNextClick(user.email!)}>
-                                        See your photos!
-                                    </Button>
-                                    :
-                                    <Button isLoading={loading} onClick={() => onNextClick(user.email!)}
-                                            disabled={!name.length}>Next</Button>
-                                }
-                            </Container>
-                        </div>)
-                ) : null
-            }
-        </section>
-    );
+              <Container>
+                <Footer>
+                  By continuing, you indicate that you have read and agree to
+                  our <Underlined>Terms of Use</Underlined> &{" "}
+                  <Underlined>Privacy Policy</Underlined>
+                </Footer>
+              </Container>
+            </Wrapper>
+          )
+        ) : (
+          <div>
+            <Header />
+            <Container>
+              <Heading>Let’s get to know you</Heading>
+              <Input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                type="text"
+                placeholder="What’s your name?"
+              />
+              {user.email ? (
+                <Button
+                  isLoading={loading}
+                  disabled={!name.length}
+                  onClick={() => onNextClick(user.email!)}
+                >
+                  See your photos!
+                </Button>
+              ) : (
+                <Button
+                  isLoading={loading}
+                  onClick={() => onNextClick(user.email!)}
+                  disabled={!name.length}
+                >
+                  Next
+                </Button>
+              )}
+            </Container>
+          </div>
+        )
+      ) : null}
+    </section>
+  );
 };
 const Heading = styled.h1`
   margin: 165px auto 0;
@@ -121,19 +130,17 @@ const Footer = styled.div`
   font-size: 14px;
   line-height: 18px;
   letter-spacing: -0.02em;
-  color: #6D6D6D;
+  color: #6d6d6d;
   margin: 0 0 38px;
   @media (min-width: 1440px) {
     font-size: 16px;
     line-height: 21px;
   }
-
 `;
 const Underlined = styled.span`
-  border-bottom: 0.75px solid #3300CC;
+  border-bottom: 0.75px solid #3300cc;
   cursor: pointer;
 `;
-
 
 const HeadingEmail = styled.h1`
   margin: 139px auto 0;
@@ -153,8 +160,8 @@ const HeadingEmail = styled.h1`
 `;
 
 const Input = styled.input`
-  background: #F4F4F4;
-  border: 1px solid #EEEEEE;
+  background: #f4f4f4;
+  border: 1px solid #eeeeee;
   border-radius: 10px;
   padding: 0 13px 0;
   height: 40px;
@@ -174,7 +181,7 @@ const Input = styled.input`
     line-height: 21px;
     display: flex;
     align-items: center;
-    color: #6D6D6D;
+    color: #6d6d6d;
   }
 
   @media (min-width: 1440px) {
